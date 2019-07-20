80aca886ff4509d77c51a987d64cba87cc854604
Matías Reinoso <matiasreinoso@est.frba.utn.edu.ar>
2019-07-20 14:16:41 -0300

    Initial commit

---
+++ app/src/main/java/mreinoso/demo_redlink/model/Album.java
@@ -0,0 +1,36 @@
+package mreinoso.demo_redlink.model;
+
+public class Album {
+
+    private int id;
+    private int userId;
+    private String title;
+
+
+    public Album() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
